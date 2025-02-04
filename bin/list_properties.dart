d053dfe54f7e105b843d0c8de6dbfda276b249a9
Hello World <tanvirofficial121@gmail.com>
2025-02-04 22:21:57 +0600

    new practice file

---
+++ bin/list_properties.dart
@@ -0,0 +1,22 @@
+void main(){
+  var listItem = ["Apple", "Banana", "Mango", "Orange",50,30,0.50,false,"hellow"];
+  var emptyList = [];
+  var singleList = [880387];
+
+  var first = listItem.first;
+  var isEmpty = emptyList.isEmpty;
+  var isNotEmpty = emptyList.isNotEmpty;
+  var length = listItem.length;
+  var last = listItem.last;
+  var reversed = listItem.reversed;
+  var single = singleList.single;
+  
+
+  print(first);
+  print(isEmpty);
+  print(isNotEmpty);
+  print(length);
+  print(last);
+  print(reversed);
+  print(single);
+}
