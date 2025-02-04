b96434ef741f261d323fbc34185afbb3ecba8481
Hello World <tanvirofficial121@gmail.com>
2025-02-04 21:44:49 +0600

    Added new Dart practice files

---
+++ bin/unary_operator.dart
@@ -0,0 +1,20 @@
+void main(){
+
+
+  var x = 10;
+
+  print(x++);// there would have initial value
+  print(x++);
+  print(x++);
+  print(x++);
+  print(x++);// that means add each times
+
+  print(++x);// start adding from this
+  print(++x);
+  print(++x);
+  print(++x);// same operation
+
+  //exactly same for --
+
+
+}
