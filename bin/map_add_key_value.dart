8273f3fc3ba32a799bbd857ae5a67a7226302266
Hello World <tanvirofficial121@gmail.com>
2025-02-05 22:32:31 +0600

    new dart practice file added 5 feb 25

---
+++ bin/map_add_key_value.dart
@@ -0,0 +1,15 @@
+void main(){
+  var mapItem = {
+    'name': 'Tanvir Emon',
+    'age': 19,
+    'id': '001',
+    'email': "tanvir@email.com"
+  };
+
+  //add a key:value
+  mapItem['mobile'] = '0177';
+
+  print(mapItem);
+
+
+}
