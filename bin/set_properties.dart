1942af708436b3740500c8d463a4228965b0fc38
Hello World <tanvirofficial121@gmail.com>
2025-02-05 22:29:52 +0600

    new dart practice file added 5 feb 25

---
+++ bin/set_properties.dart
@@ -0,0 +1,13 @@
+void main(){
+  var myCitySet = <String>{'Dhaka','Ctg','Barishal','Laksmipur','Cumilla'};
+  var singleSet = <String>{'Feni'};
+  print(myCitySet.first);
+  print(myCitySet.isEmpty);
+  print(myCitySet.isNotEmpty);
+  print(myCitySet.length);
+  print(myCitySet.last);
+  print(singleSet.single);
+  print(singleSet.hashCode);
+  print(myCitySet.hashCode);
+
+}
